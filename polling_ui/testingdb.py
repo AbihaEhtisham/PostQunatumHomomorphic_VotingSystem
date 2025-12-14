1399c50739c22e92fde38e317c6de23adafdb1f8
Eman <nameemanfatima@gmail.com>
2025-12-14 13:01:57 +0500

    push latets to main

--- polling_ui/testingdb.py
+++ polling_ui/testingdb.py
@@ -4,7 +4,7 @@ DBV_PATH = r"D:\PostQunatumHomomorphic_VotingSystem\polling_ui\votes.db"
 conn = sqlite3.connect(DBV_PATH)
 c = conn.cursor()
 c.execute("SELECT id, cnic, candidate, bfv_cipher, timestamp FROM votes")
-rows = c.fetchall()
+rows = c.fetchall()   
 for row in rows:
     print(row)
 conn.close()
